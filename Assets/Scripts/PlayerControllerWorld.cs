d1740232375393cd340b09da615c298d49dc4c1e
kaldrick <kaldrick@gmail.com>
2013-09-29 13:04:15 +0200

    Zoom+Fog+Skybox

--- Assets/Scripts/PlayerControllerWorld.cs
+++ Assets/Scripts/PlayerControllerWorld.cs
@@ -4,91 +4,79 @@ using System.Collections;
 public class PlayerControllerWorld : MonoBehaviour {
 	private Vector3 newPos;
 	private bool bChangeCamera;
-	public GameObject bulletPrefab;
-	public GameObject[] bullets;
 	public Camera normalCam, overTopCam;
+	public float fZoom = 0.0f;
+	public float posY, posZ, rotX, prevPosY;
+	public Vector3 camPos, camRot, startPos, startRot;
 	// Use this for initialization
 	void Start () {
 		rigidbody.freezeRotation = true;
 		Physics.gravity = new Vector3(0f, -256.0f, 0f);
-		bullets = new GameObject[100];
 		normalCam.enabled = true;
 		overTopCam.enabled = false;
-		for(int i=0; i<100; i++)
-		{
-			bullets[i] = Instantiate (bulletPrefab, GameObject.Find("GunPoint").transform.position, GameObject.Find("GunPoint").transform.rotation) as GameObject;
-			bullets[i].GetComponent<Bullet>().forward = transform.forward;
-			Physics.IgnoreCollision (bullets[i].collider, collider);
-			bullets[i].transform.parent = GameObject.Find ("Bullets").transform;
-		}
+		camPos = new Vector3(0, 32, -8);
+		camRot = new Vector3(60, 0, 0);
+		posY = 32f;
+		posZ = -8f;
+		rotX = 60f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float rotationInput = Input.GetAxis("Horizontal") * Time.deltaTime * 128;
 		float moveInput = Input.GetAxis ("Vertical") * Time.deltaTime * 1024;
+		float scrollInput = -Input.GetAxis ("Mouse ScrollWheel");
+		if(scrollInput != 0f)
+		{
+			fZoom = Mathf.Clamp (fZoom + scrollInput * 10f, -10f, 10f);
+			posY = Mathf.Clamp (camPos.y - fZoom * 6f, 14f, 32f);
+			posZ = Mathf.Clamp (camPos.z - fZoom*2f, -18f, -8f);
+			rotX = Mathf.Clamp (normalCam.transform.localEulerAngles.x - fZoom * 12f, 24f, 60f);
+			Debug.Log (fZoom);
+			/*if(normalCam.enabled && fZoom != 0f)
+			{
+				normalCam.transform.localPosition = new Vector3(normalCam.transform.localPosition.x, Mathf.Clamp (normalCam.transform.localPosition.y - fZoom * 6f, 8f, 32f), Mathf.Clamp (normalCam.transform.localPosition.z - fZoom*2f, -24f, -8f));
+				normalCam.transform.localEulerAngles = new Vector3 (Mathf.Clamp (normalCam.transform.localEulerAngles.x - fZoom * 12f, 0f, 60f), normalCam.transform.localEulerAngles.y, normalCam.transform.localEulerAngles.z);
+			}*/
+		}
+		else
+		{
+			fZoom = 0f;
+		}
+		camPos.y = Mathf.Lerp (normalCam.transform.localPosition.y, posY, Time.deltaTime * 4.5f);
+		camPos.z = Mathf.Lerp (normalCam.transform.localPosition.z, posZ, Time.deltaTime * 4.5f);
+		camRot.x = Mathf.Lerp (normalCam.transform.localEulerAngles.x, rotX, Time.deltaTime * 4.5f);
+		normalCam.transform.localPosition = camPos;
+		normalCam.transform.localEulerAngles = camRot;	
+		Debug.Log (fZoom);
+		/*if(normalCam.enabled && fZoom != 0f)
+		{
+			normalCam.transform.localPosition = new Vector3(normalCam.transform.localPosition.x, Mathf.Clamp (normalCam.transform.localPosition.y - fZoom * 6f, 14f, 32f), Mathf.Clamp (normalCam.transform.localPosition.z - fZoom*2f, -18f, -8f));
+			normalCam.transform.localEulerAngles = new Vector3 (Mathf.Clamp (normalCam.transform.localEulerAngles.x - fZoom * 12f, 24f, 60f), normalCam.transform.localEulerAngles.y, normalCam.transform.localEulerAngles.z);
+		}*/
 		if (Input.GetKeyDown(KeyCode.F2)) 
 		{
 			if(normalCam.enabled)
 			{
 				normalCam.enabled = false;
 				overTopCam.enabled = true;
+				prevPosY = transform.localPosition.y;
+				transform.localScale = new Vector3(64, 1024, 64);
+				RenderSettings.fog = false;
 			}
 			else
 			{
 				normalCam.enabled = true;
 				overTopCam.enabled = false;
+				transform.localScale = new Vector3(1, 1, 1);
+				transform.localPosition = new Vector3(transform.localPosition.x, prevPosY + 5f, transform.localPosition.z);
+				RenderSettings.fog = true;
 			}
 		}
-		//transform.Translate(moveVector);
 		rigidbody.velocity = transform.forward * moveInput;
 		newPos = transform.position;
-	//	newPos.y = 1.5f;
 		transform.position = newPos;
 		transform.Rotate(new Vector3(0, rotationInput, 0));
-	/*	if(Input.GetKeyDown (KeyCode.Tab))
-		{
-			bChangeCamera = !bChangeCamera;
-		}
-		if(!bChangeCamera)
-		{
-			Camera.mainCamera.depth = -1;
-			Camera.mainCamera.farClipPlane = 0.02f;
-		}
-		else
-		{
-			Camera.mainCamera.depth = 1;
-			Camera.mainCamera.farClipPlane = 1000;
-		}*/
-		if(Input.GetButtonDown("Fire1"))
-		{
-			FireBullet();
-		}
-	}
-	void FireBullet()
-	{
-		int bulletNum = -1;
-		for(int i=0; i<100;i++)
-		{
-			if(bullets[i].activeSelf == false) 
-		    {
-				bulletNum=i;
-				break; 
-			}
-		}
-		// found one (if we couldn't find unused orc, no spawn):
-		if(bulletNum>=0) 
-		{ 
-			bullets[bulletNum].SetActive(true);
-			bullets[bulletNum].GetComponent<Bullet>().forward = transform.forward;
-			bullets[bulletNum].transform.position = GameObject.Find("GunPoint").transform.position;
-			bullets[bulletNum].transform.rotation = GameObject.Find("GunPoint").transform.rotation;
-			bullets[bulletNum].rigidbody.velocity = new Vector3(0,0,0);
-			Physics.IgnoreCollision (bullets[bulletNum].collider, collider);
-			// activate this orc, move it to spawn point:
-			
-			
-		}
 	}
 	void OnCollisionEnter(Collision other)
 	{
--- Assets/Shaders/Normal-Diffuse.shader
+++ Assets/Shaders/Normal-Diffuse.shader
@@ -9,7 +9,7 @@ SubShader {
 	LOD 200
 
 CGPROGRAM
-#pragma surface surf Lambert
+#pragma surface surf Lambert vertex:vert
 
 sampler2D _MainTex;
 sampler2D _BumpMap;
@@ -19,16 +19,24 @@ struct Input {
 	float2 uv_MainTex;
 	float2 uv_BumpMap;
 	float4 color : COLOR;
+	float3 vertColor;
 };
+void vert (inout appdata_full v, out Input o) {
 
+    UNITY_INITIALIZE_OUTPUT(Input, o);
+
+    o.vertColor = v.color;
+
+}
 void surf (Input IN, inout SurfaceOutput o) {
-	fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
-	o.Albedo = c.rgb * IN.color.rgb;
+	fixed4 c = tex2D(_MainTex, IN.uv_MainTex) *  _Color;
+	
+	o.Albedo = c.rgb * IN.vertColor;
 	o.Alpha = c.a;
 	o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
 }
 ENDCG
 }
 
-Fallback "VertexLit"
+Fallback "Diffuse"
 }
